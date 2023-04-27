da87f33ce38d0a87d2ce388b7759f577296fca12
michaeltitze <91420291+michaeltitze@users.noreply.github.com>
2023-04-27 17:11:18 +0200

    make it 404 work

--- mkvc-text-gen/src/App.js
+++ mkvc-text-gen/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
+    <Router basename="/mkvc-text-gen">
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +22,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Router>
   );
 }
 
